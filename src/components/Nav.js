039c4760284f3b6b4f4fe428511f14a1be74b201
jxxuxuefeng <jxxuxuefeng@qq.com>
2019-08-11 22:42:21 +0800

    feater: webpack增加css解析

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 import React, { useContext, PureComponent } from 'react';
 import { BookContext } from '../context/BookContext';
+import './style.css';
 
 function Nav() {
   const { books } = useContext(BookContext);
@@ -11,7 +12,7 @@ class NavDetail extends PureComponent {
   render() {
     return (
       <nav>
-        <h3 className="222">{this.props.name}</h3>
+        <h3 className="xx">{this.props.name}</h3>
         <p>书本列表: {this.props.num}</p>
       </nav>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
         test: /\.js$/,
         use: ['babel-loader', 'eslint-loader'],
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
